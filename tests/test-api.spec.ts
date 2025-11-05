8df34d085d7fa1e705e7a1fe0919cb6a7cb488e0
Theerapat.Demo <theerapat.demo@gmail.com>
2025-11-05 21:28:15 +0700

    add script test

---
+++ tests/test-api.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test.only('GET list', async ({ request }) => {
+    const res = await request.get('http://srv946485.hstgr.cloud:8080/api/v1/customers');
+    expect(res.ok()).toBeTruthy();
+
+    expect(res.ok()).toBeTruthy();
+
+    const body = await res.json();
+    console.log(body);
+
+    // response must be array
+    expect(Array.isArray(body)).toBeTruthy();
+    expect(body.length).toBeGreaterThan(0);
+
+    // check first item
+    const item = body[0];
+    expect(item).toHaveProperty('customer_id');
+    expect(item).toHaveProperty('first_name');
+    expect(item).toHaveProperty('last_name');
+    expect(item).toHaveProperty('email');
+    expect(item).toHaveProperty('gender');
+    expect(item).toHaveProperty('status');
+
+    // specific expected
+    expect(['Male', 'Female', 'Other']).toContain(item.gender);
+    expect(['active', 'inactive', 'suspended']).toContain(item.status);
+
+});
